9414c98e513ffe09ed1991eccaa682ac5b670f04
Lisa Huang <lisa.yc.huang@gmail.com>
2019-01-07 17:32:29 -0800

    Create index view and storeController

--- delicious-node/routes/index.js
+++ delicious-node/routes/index.js
@@ -1,13 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const storeController = require('../controllers/storeController')
 
-// Do work here
-router.get('/', (req, res) => {
-  // res.send('Hey! It works!');
-  res.render('hello', {
-    name: "Lisa",
-    dog: "Fido"
-  });
-});
+router.get('/', storeController.homePage);
 
 module.exports = router;
